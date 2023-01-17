3a8d4266978dde9c447429319d17a9cee7b37590
Rakit Dautov <rocke1995@gmail.com>
2023-01-17 22:04:56 +0600

    test

--- factory_bot_r/settings.py
+++ factory_bot_r/settings.py
@@ -33,6 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "web",
+    "*",
 ]
 
 
@@ -87,13 +88,9 @@ WSGI_APPLICATION = "factory_bot_r.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": os.getenv("DB_ENGINE"),
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
